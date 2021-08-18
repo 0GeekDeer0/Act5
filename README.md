889522979ef88a91f691b9f101f4622477f85439
Diego Sánchez Luna <89027006+0GeekDeer0@users.noreply.github.com>
2021-08-17 21:34:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+![Tec de Monterrey](images/logotecmty.png)
+# Ejercicios Actividad 5!
+
+- 01 Diagonal Rectángulo
+- 02 Cantidad de Litros de Pintura
+- 03 Distancia Dos Puntos
+- 04 Escalera
+- 05 Crecimiento de población
---
+++ assignments/01DiagonalRect/src/exercise.py
@@ -0,0 +1,14 @@
+def main():
+    #escribe tu código abajo de esta línea
+    #Lee los datos
+    #escribe tu código abajo de esta línea
+    altura=float(input())
+    angulo=int(input()
+    radianes=math.radians(angulo)
+    largo=round(altura/math.sin(radianes))
+    print(f"{largo}")
+
+
+
+if __name__ == '__main__':
+    main()
